89381e5e5581e465ffce9a46951f12ce64c896d3
ivanesc25 <ivan.escalante.m@gmail.com>
2019-06-14 13:29:26 -0500

    Ya funciona el dropdown y hide mantenieno valores

--- nova-tree/src/components/nova-tree/nova-tree.tsx
+++ nova-tree/src/components/nova-tree/nova-tree.tsx
@@ -6,47 +6,44 @@ import { Component, h } from "@stencil/core";
   styleUrl: "nova-tree.scss",
   shadow: true
 })
-
-
 export class MyComponent {
-
-   changeClass =( ) =>{
-     //window.alert("hola") <- estaba para ver si entra, si entra
-    var toggler = document.getElementsByClassName("caret");
-    var i;
-
-    for (i = 0; i < toggler.length; i++) {
-      toggler[i].addEventListener("click", function() {
-        this.parentElement.querySelector(".nested").classList.toggle("active");
-        this.classList.toggle("caret-down");
-      });
-}
-
-  }
-
+  handleToggle = e => {
+    e.target.classList.toggle("caret-down");
+    e.target.nextElementSibling.classList.toggle("active");
+  };
 
   render() {
-    return (<ul id="myUL">
-      <li><span class="caret" onClick={() => this.changeClass()}>Beverages</span>
-        <ul class="nested">
-          <li>Water</li>
-          <li>Coffee</li>
-          <li><span class="caret">Tea</span>
-            <ul class="nested">
-              <li>Black Tea</li>
-              <li>White Tea</li>
-              <li><span class="caret">Green Tea</span>
-                <ul class="nested">
-                  <li>Sencha</li>
-                  <li>Gyokuro</li>
-                  <li>Matcha</li>
-                  <li>Pi Lo Chun</li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </li>
-    </ul>)
-    }
+    return (
+      <ul id="myUL">
+        <li>
+          <span class="caret" onClick={e => this.handleToggle(e)}>
+            Beverages
+          </span>
+          <ul class="nested">
+            <li>Water</li>
+            <li>Coffee</li>
+            <li>
+              <span class="caret" onClick={e => this.handleToggle(e)}>
+                Tea
+              </span>
+              <ul class="nested">
+                <li>Black Tea</li>
+                <li>White Tea</li>
+                <li>
+                  <span class="caret" onClick={e => this.handleToggle(e)}>
+                  Green Tea</span>
+                  <ul class="nested">
+                    <li>Sencha</li>
+                    <li>Gyokuro</li>
+                    <li>Matcha</li>
+                    <li>Pi Lo Chun</li>
+                  </ul>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    );
+  }
 }

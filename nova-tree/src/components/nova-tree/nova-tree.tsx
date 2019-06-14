9eadc798d18ed1261dca26b07df86f0a43aa8310
ivanesc25 <ivan.escalante.m@gmail.com>
2019-06-14 12:22:11 -0500

    Subiendo la funcion en vanilla a tsx

--- nova-tree/src/components/nova-tree/nova-tree.tsx
+++ nova-tree/src/components/nova-tree/nova-tree.tsx
@@ -6,88 +6,55 @@ import Fragment from "stencil-fragment";
   styleUrl: "nova-tree.scss",
   shadow: true
 })
+
+
 export class MyComponent {
 
+  function N_name() {
+    var toggler = document.getElementsByClassName("caret");
+    var i;
 
+    for (i = 0; i < toggler.length; i++) {
+      toggler[i].addEventListener("click", function() {
+        this.parentElement.querySelector(".nested").classList.toggle("active");
+        this.classList.toggle("caret-down");
+      });
+}
 
+  }
 
 
   render() {
-
-
-    let treeData = [
-      {
-        title: "0-0",
-        key: "0-0",
-        checked: true,
-        children: [
-                  {
-                    title: "0-0-0",
-                    key: "0-0-0",
-                    checked: true,
-                    children: [
-                              { title: "0-0-0-0", key: "0-0-0-0", checked: true },
-                              { title: "0-0-0-1", key: "0-0-0-1", checked: true },
-                              { title: "0-0-0-2", key: "0-0-0-2", checked: false }
-                              ]
-                  },
-                  {
-                    title: "0-0-1",
-                    key: "0-0-1",
-                    checked: false,
-                    children: [
-                              { title: "0-0-1-0", key: "0-0-1-0", checked: false },
-                              { title: "0-0-1-1", key: "0-0-1-1", checked: false },
-                              { title: "0-0-1-2", key: "0-0-1-2", checked: false }
-                              ]
-                  },
-                  {
-                    title: "0-0-2",
-                    key: "0-0-2",
-                    checked: false
-                  }
-                  ]
-      },
-      {
-        title: "0-1", key: "0-1",checked: false,
-        children: [
-                  { title: "0-1-0-0", key: "0-1-0-0", checked: false },
-                  { title: "0-1-0-1", key: "0-1-0-1", checked: false },
-                  { title: "0-1-0-2", key: "0-1-0-2", checked: false }
-                  ]
-      },
-      {
-        title: "0-2",
-        key: "0-2",
-        checked: false
-      }
-    ];
-
 
    //if leaf checked false  => children checked false
    //if leaf checked true => children are individual
    // lo quitamos con un class="float hide noselect" <- hide
 
+   //poniendo <script src="./index.js"></script> como funcion
 
-    const handleNode = leaf => {
-      return (
-        <Fragment>
-          <nova-tree-node
-            key={leaf.key}
-            text={leaf.title}
-            checked={leaf.checked}
-          />
 
-          {leaf.children ? (
-            <div class="children">
-              {leaf.children.map(child => handleNode(child))}
-            </div>
-          ) : (
-            undefined
-          )}
-        </Fragment>
-      );
-    };
-    return <ul>{treeData.map(leaf => handleNode(leaf))}</ul>;
-  }
+
+    return (<ul id="myUL">
+      <li><span class="caret">Beverages</span>
+        <ul class="nested">
+          <li>Water</li>
+          <li>Coffee</li>
+          <li><span class="caret">Tea</span>
+            <ul class="nested">
+              <li>Black Tea</li>
+              <li>White Tea</li>
+              <li><span class="caret">Green Tea</span>
+                <ul class="nested">
+                  <li>Sencha</li>
+                  <li>Gyokuro</li>
+                  <li>Matcha</li>
+                  <li>Pi Lo Chun</li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>)
+    }
 }

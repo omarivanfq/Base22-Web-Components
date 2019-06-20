80c021e8b4e55690b9480323ef764274b861bb11
Ivanesc25 <ivan.esccalante.m@gmail.com>
2019-06-20 14:13:15 -0500

    Mandaba un error en el toggle del caret, ya se corrigio

--- nova-tree/src/components/nova-checkbox/nova-checkbox.tsx
+++ nova-tree/src/components/nova-checkbox/nova-checkbox.tsx
@@ -1,31 +1,31 @@
-import { Component, Event, EventEmitter, h, Prop } from '@stencil/core';
+import { Component, Event, EventEmitter, h, Prop } from "@stencil/core";
 
 @Component({
-    tag: 'nova-checkbox',
-    styleUrl: 'nova-checkbox.css',
-    shadow: true
+  tag: "nova-checkbox",
+  styleUrl: "nova-checkbox.css",
+  shadow: true
 })
-
 export class NovaCheckbox {
-    @Prop() checked:boolean;
-    @Prop() disabled:boolean;
-    @Prop() styles:any = {};
-    @Event() clicked:EventEmitter;
-    @Prop() handleClick:Function = () => {};
-    render() {
-        return (
-            <div class="wrapper" style={this.styles}>
-                <label class={ "container " + (this.disabled? "disabled" : "")}>
-                    <input
-                        type="checkbox"
-                        {...{checked: this.checked, disabled: this.disabled}}
-                        onClick={() => {
-                            this.clicked.emit();
-                            this.handleClick();
-                        }}/>
-                    <span class="checkmark"></span>
-                 </label>
-            </div>
-        );
-    }
+  @Prop() checked: boolean;
+  @Prop() disabled: boolean;
+  @Prop() styles: any = {};
+  @Event() clicked: EventEmitter;
+  @Prop() handleClick: Function = () => {};
+  render() {
+    return (
+      <div class="wrapper" style={this.styles}>
+        <label class={"container " + (this.disabled ? "disabled" : "")}>
+          <input
+            type="checkbox"
+            {...{ checked: this.checked, disabled: this.disabled }}
+            onClick={() => {
+              this.clicked.emit();
+              this.handleClick();
+            }}
+          />
+          <span class="checkmark"></span>
+        </label>
+      </div>
+    );
+  }
 }
--- nova-tree/src/components/nova-tree/nova-tree.tsx
+++ nova-tree/src/components/nova-tree/nova-tree.tsx
@@ -1,4 +1,11 @@
-import { Component, Prop, Element, Event, EventEmitter, h } from "@stencil/core";
+import {
+  Component,
+  Prop,
+  Element,
+  Event,
+  EventEmitter,
+  h
+} from "@stencil/core";
 //import Fragment from "stencil-fragment";
 // State, <- ete metelo con los demas
 //method para las cosas que se pueden llamar desde auera porque son publicas
@@ -12,39 +19,43 @@ import { Component, Prop, Element, Event, EventEmitter, h } from "@stencil/core"
 export class MyComponent {
   @Element() private element: HTMLElement;
 
-//este de abajo es de stencil corregido despues de meterlo dentro del wrapper
+  //este de abajo es de stencil corregido despues de meterlo dentro del wrapper
   componentDidLoad() {
-     let ul = this.element.shadowRoot.children.item(1);
-     this.autoExpandParent ? this.handleAutoExpand(ul) : undefined;
-   }
-   handleAutoExpand = e => {
-     let uls = e.getElementsByTagName("ul");
-     for (var i = 0; i < uls.length; i++) {
-       uls.item(i).classList.add("active");
-     }
-   };
-   handleToggle = e => {
-     e.target.classList.toggle("caret-down");
-     //e.target.nextElementSibling.classList.toggle("active");
-     //e.target.children[1].classList.toggle("active");
-     //es primero el 0 checbkos y despues el 1 porque ya es el ul
-     //tiene que ser un child element de li
-     e.target.parentNode.lastChild.classList.toggle("active");
-     console.log(e.target.parentNode);
-   };
-   /*handleClick(){
+    let ul = this.element.shadowRoot.children.item(1);
+    this.autoExpandParent ? this.handleAutoExpand(ul) : undefined;
+  }
+  handleAutoExpand = e => {
+    let uls = e.getElementsByTagName("ul");
+    for (var i = 0; i < uls.length; i++) {
+      uls.item(i).classList.add("active");
+    }
+  };
+  handleToggle = e => {
+    e.target.classList.toggle("caret-down");
+    //e.target.nextElementSibling.classList.toggle("active");
+    //e.target.children[1].classList.toggle("active");
+    //es primero el 0 checbkos y despues el 1 porque ya es el ul
+    //tiene que ser un child element de li
+    e.target.parentNode.lastChild.classList.toggle("active");
+
+    //console.log(e.target.parentNode);
+    //console.log("el de abajo es el que buscas");
+    //console.log(e.target.parentNode.lastChild.classList);
+  };
+  /*handleClick(){
      console.log("itworked");
    }*/
 
- @Prop() checked:boolean;
- @Prop() disabled:boolean;
- @Prop() styles:any = {};
- @Event() clicked:EventEmitter;
- @Prop() handleClick:Function = () => {console.log("itworked");};
- @Prop() autoExpandParent: boolean = true ;
- @Prop() checkStrictly: boolean = true;
- //checkStrictly debe ser false
-
+  @Prop() checked: boolean;
+  @Prop() disabled: boolean;
+  @Prop() styles: any = {};
+  @Event() clicked: EventEmitter;
+  @Prop() handleClick: Function = () => {
+    console.log("itworked");
+  };
+  @Prop() autoExpandParent: boolean = true;
+  @Prop() checkStrictly: boolean = true;
+  //checkStrictly debe ser false
 
   render() {
     return (
@@ -55,52 +66,54 @@ export class MyComponent {
       //esto es con la intencino de quemetas el WILL load, arapra que no haga tnto overhead
 
       <ul id="myUL">
-      <li>
-       <span class = "caret" onClick={e =>{ this.handleToggle(e);  console.log("una")}}></span>
-            <nova-checkbox></nova-checkbox>happy
-             <ul class="nested">
-                 <li class = "caretsecret"><nova-checkbox></nova-checkbox>Water
-                 </li>
+        <li>
+          <span class="caret" onClick={e => this.handleToggle(e)}></span>
+          <nova-checkbox></nova-checkbox>happy
+          <ul class="nested">
+            <li class="caretsecret">
+              <nova-checkbox></nova-checkbox>Water
+            </li>
 
-                 <li>
-                 <span class = "caret" onClick={e =>{ this.handleToggle(e);  console.log("una")}}></span>
-                 <nova-checkbox></nova-checkbox>Coffee
-                      <ul class ="nested">
-                          <li class ="caretsecret"><nova-checkbox></nova-checkbox>Coffe
-                          </li>
-                          <li><span class = "caret" onClick={e =>{ this.handleToggle(e);  console.log("una")}}></span>
-                          <nova-checkbox></nova-checkbox>Whatever
-                          </li>
-                      </ul>
-                    </li>
-                </ul>
-      </li>
-
-        <li> <span class = "caret" onClick={e =>{ this.handleToggle(e);  console.log("una")}}></span><nova-checkbox></nova-checkbox>Beverages
-             <ul class="nested">
-                 <li class="caretsecret">
-                 <nova-checkbox></nova-checkbox>Water
-                 </li>
-
-                 <li class="caretsecret">
-                 <nova-checkbox></nova-checkbox>Coffee
-                 </li>
-
-                 <li >
-                  <span class = "caret" onClick={e =>{ this.handleToggle(e);  console.log("una")}}></span>
-                 <nova-checkbox></nova-checkbox>Tea
-
-                 </li>
-             </ul>
+            <li>
+              <span class="caret" onClick={e => this.handleToggle(e)}></span>
+              <nova-checkbox></nova-checkbox>Coffee
+              <ul class="nested">
+                <li class="caretsecret">
+                  <nova-checkbox></nova-checkbox>Coffe
+                </li>
+                <li>
+                  <span
+                    class="caret"
+                    onClick={e => this.handleToggle(e)}
+                  ></span>
+                  <nova-checkbox></nova-checkbox>Whatever
+                </li>
+              </ul>
+            </li>
+          </ul>
         </li>
 
+        <li>
+          <span class="caret" onClick={e => this.handleToggle(e)}></span>
+          <nova-checkbox></nova-checkbox>Beverages
+          <ul class="nested">
+            <li class="caretsecret">
+              <nova-checkbox></nova-checkbox>Water
+            </li>
+
+            <li class="caretsecret">
+              <nova-checkbox></nova-checkbox>Coffee
+            </li>
+
+            <li>
+              <span class="caret" onClick={e => this.handleToggle(e)}></span>
+              <nova-checkbox></nova-checkbox>Tea
+            </li>
+          </ul>
+        </li>
       </ul>
 
-
-
-
-
-//no toques aqui abajo
+      //no toques aqui abajo
     );
-    }
-    }
+  }
+}
